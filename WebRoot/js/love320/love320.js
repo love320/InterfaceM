ed21ac820fb3f400b28d3be3e6dd83af8a01107d
zhangdi <admin@love320.com>
2012-10-04 00:38:19 +0800

    处理json获取乱码问题
    Signed-off-by: zhangdi <admin@love320.com>

--- WebRoot/js/love320/love320.js
+++ WebRoot/js/love320/love320.js
@@ -2,15 +2,16 @@
 $(function(){
 	 //set template and process
 	var paralisturl = $("#paralisturl").attr("value");
-    $("#paradiv").setTemplateElement("Template-Items").processTemplateURL(paralisturl);
+	/**处理编码（有乱码）*/
+    //$("#paradiv").setTemplateElement("Template-Items").processTemplateURL(paralisturl);
 	
-	/*$.getJSON(paralisturl, function(json){
+	$.getJSON(paralisturl, function(json){
 		 // 设置模板  
           $("#paradiv").setTemplateElement("Template-Items");  
           // 处理模板  
           $("#paradiv").processTemplate(json);  
 		});
-	*/
+	
 });
 
 function parameterSavePost(data){
